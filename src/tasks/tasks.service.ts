f72cd45058c16fe44e73fda0f36afa13b260407e
Fortis <fortispotens@gmail.com>
2021-03-27 22:01:54 +0100

    setup update task, delete task, search and filter tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { Injectable } from '@nestjs/common';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,26 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto) {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (search) {
+      tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search));
+    }
+
+    if (status) {
+      tasks = tasks.filter(task => task.status === status)
+    }
+
+    return tasks;
+  }
+
+  getTaskById(id: string) {
+    return this.tasks.find(task => task.id === id)
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -23,5 +44,17 @@ export class TasksService {
 
     this.tasks.push(task);
     return task;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id)
+    task.status = status
+
+    return task;
+
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter(task => task.id !== id)
   }
 }
