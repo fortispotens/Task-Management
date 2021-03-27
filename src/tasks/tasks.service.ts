0b37aca98bc4cd6f323125da1a2f0da12fdde48f
Fortis <fortispotens@gmail.com>
2021-03-27 17:35:05 +0100

    setup task model, getAllTasks and CreateTask methods using in-memory data

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
+import { Task, TaskStatus } from './task.model';
+import { v1 as uuid } from 'uuid';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+}
